a495cef6a0d57a02894775039dfac861b69bb3b7
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-15 19:16:21 -0600

    heroku changes

--- App/__init__.py
+++ App/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app)
+# CORS(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
